72565cef87ece951025e248a479dba7764aeed29
Smit Khobragade <smitkbgd@gmail.com>
2025-01-17 16:51:47 +0530

    DataBase Configured with ENV

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import service.ParkingService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Parking System");
+        ParkingService parkingservice = new ParkingService();
+        parkingservice.startCLI();
     }
 }
